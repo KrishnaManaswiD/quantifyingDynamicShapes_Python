4ab48458e77ece2ba3f28b5b172b7e3da070f068
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2022-05-25 00:39:05 +0200

    corrected and tested saveFrames code

--- save_frames.py
+++ save_frames.py
@@ -5,20 +5,17 @@
 # Date: May 2022
 #
 # I would appreciate it if you cite the following paper when using this code 
-# Digumarti KM, Trimmer B, Conn AT, Rossiter J. 
-# "Quantifying Dynamic Shapes in Soft Morphologies."
-# Soft Robotics. 6(6), pp.733-744. 2019
+# Digumarti KM, Trimmer B, Conn AT, Rossiter J. Quantifying Dynamic Shapes in Soft Morphologies. Soft Robotics. 2019 Dec 1;6(6):733-44.
 
 import os
 import cv2
 
 ## construct subfolder to save frames from movie
 folderForFrames = 'frames'
-recordingName = 'movie'           
-
-## create sub folder
+recordingName = 'movie'         # set this to the name of the video file
 subFolder = folderForFrames + '/' + recordingName
 
+## create sub folder if it does not exist
 if not os.path.exists(subFolder):
     os.makedirs(subFolder)
 
@@ -27,14 +24,13 @@ v = cv2.VideoCapture(recordingName + '.mp4')
 frameNum = 0
 
 while v.isOpened():
-    ret, frame = v.read()
-
-    if ret == True:
+    success, frame = v.read()
+    
+    if success:
         frameNum += 1
-        cv2.imwrite(os.path.join(subfolder + '/' + frameNum), frame)
+        cv2.imwrite(os.path.join(subFolder, "frame{:d}.png".format(frameNum)), frame)
     else:
         break
-end
 
 v.release()
 cv2.destroyAllWindows()
